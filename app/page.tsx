fe89a9b54d2e3b735101f145d345cd1696c3186e
Diogo Jorge <diogofernandoj@gmail.com>
2024-12-13 14:27:11 -0300

    feat: add initial HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-const HomePage = () => {
-  return <h1>Hello world!</h1>;
+import { FlagIcon } from "lucide-react";
+import CountryItem from "./_components/country-item";
+import { getCountries } from "./_data-access/get-countries";
+
+const HomePage = async () => {
+  const countries = await getCountries();
+
+  return (
+    <div className="flex flex-col gap-8 items-center p-8">
+      <h1 className="flex items-center gap-1 font-bold">
+        Country Info App <FlagIcon />
+      </h1>
+
+      <div className="flex justify-center flex-wrap gap-5">
+        {countries.map((country) => (
+          <CountryItem key={country.countryCode} country={country} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
