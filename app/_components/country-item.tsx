589d8f2364d1267e1edefbfab270e4c888bafa85
Diogo Jorge <diogofernandoj@gmail.com>
2024-12-13 14:27:04 -0300

    feat: add initial CountryItem

---
+++ app/_components/country-item.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import { CountryDTO } from "../_data-access/get-countries";
+
+interface CountryItemProps {
+  country: CountryDTO;
+}
+
+const CountryItem = ({ country }: CountryItemProps) => {
+  return (
+    <Link href={`/countries/${country.countryCode}`}>
+      <div className="bg-white p-4 rounded-md flex flex-col gap-2">
+        <Image
+          src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country.countryCode}.svg`}
+          alt={country.name}
+          height={60}
+          width={180}
+        />
+        <p className="font-semibold text-center">{country.name}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default CountryItem;
